3108d519f600f6b5ef4a82d3f5b148d0d33944ee
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-22 23:59:43 -0500

    Event listeners for numbers vs operators

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const num = document.querySelectorAll('.number');
+const op = document.querySelectorAll('.operator');
 const answer = document.getElementById('answer');
 console.log(answer);
+let valOne = null;
+let valTwo = null;
+let storedOp = null;
 
 num.forEach((button) => {
     button.addEventListener('click', function(e){
-        let val = button.value;
-        answer.innerHTML = val;
+        if (valOne == null && storedOp == null){
+            valOne = button.value;
+        } else {
+            
+        }
+        answer.innerHTML = storedVal;
+    });
+});
+
+op.forEach((button) => {
+    button.addEventListener('click', function(e){
+        if (valTwo != null){
+            return equation(valOne, valTwo, storedOp);
+        }
+        storedOp = button.value;
     });
 });
