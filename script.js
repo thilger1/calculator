ef570eab68b19ecf61ecd145eb2887743140e857
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-23 12:22:06 -0500

    valOne and valTwo stored

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 const num = document.querySelectorAll('.number');
 const op = document.querySelectorAll('.operator');
 const answer = document.getElementById('answer');
-console.log(answer);
+const clear = document.getElementById('clearButton');
 let valOne = null;
 let valTwo = null;
 let storedOp = null;
+let fullVal = null;
+
+clear.addEventListener('click', () => {
+    valOne = null;
+    valTwo = null;
+    storedOp = null;
+    answer.innerHTML = "";
+});
 
 num.forEach((button) => {
     button.addEventListener('click', function(e){
-        if (valOne == null && storedOp == null){
-            valOne = button.value;
+        newVal = button.value;
+        //val Two (after operator)
+        if (valOne != null && storedOp != null){
+            if (valTwo == null){
+                valTwo = newVal;
+            } else {
+                valTwo = valTwo.concat(newVal);
+            }
+            answer.innerHTML = valTwo;
+        //val One (before operator)
         } else {
-            
+            if (valOne == null){
+                valOne = newVal;
+            } else {
+                valOne = valOne.concat(newVal);
+            }
+            answer.innerHTML = valOne;
         }
-        answer.innerHTML = storedVal;
     });
 });
 
 op.forEach((button) => {
     button.addEventListener('click', function(e){
         if (valTwo != null){
-            return equation(valOne, valTwo, storedOp);
+            answer.innerHTML = equation(valOne, valTwo, storedOp);
         }
-        storedOp = button.value;
+        else {
+            storedOp = button.value;
+            answer.innerHTML = storedOp;
+        }
     });
 });
+
+function equation(valOne, valTwo, storedOp) {
+    switch (storedOp) {
+        case '+':
+            valOne = valOne + valTwo;
+            valTwo = null;
+            answer.innerHTML = valOne;
+            break;
+        case '-':
+            valOne = valOne - valTwo;
+            valTwo = null;
+            answer.innerHTML = valOne;
+            break;
+        case '/':
+            valOne = valOne / valTwo;
+            valTwo = null;
+            answer.innerHTML = valOne;
+            break;
+        case '*':
+            valOne = valOne * valTwo;
+            valTwo = null;
+            answer.innerHTML = valOne;
+            break;
+    }
+};
