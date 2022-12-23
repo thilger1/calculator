a02409be78b0fec6aa62612cffdae8d6cba33931
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-22 23:44:09 -0500

    Add number class

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const btn = document.querySelectorAll("button");
+const num = document.querySelectorAll('.number');
 const answer = document.getElementById('answer');
 console.log(answer);
 
-btn.forEach((button) => {
+num.forEach((button) => {
     button.addEventListener('click', function(e){
         let val = button.value;
         answer.innerHTML = val;
