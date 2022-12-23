a0b4ded7528de12ec23c78d30b6a58a1df833a9d
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-22 23:31:43 -0500

    Numbers have values, populate in answer when clicked

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const body = document.querySelector('body');
+const btn = document.querySelectorAll("button");
+const answer = document.getElementById('answer');
+console.log(answer);
+
+btn.forEach((button) => {
+    button.addEventListener('click', function(e){
+        let val = button.value;
+        answer.innerHTML = val;
+    });
+});
