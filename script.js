150c58979cea7bea5b5615f05c621fc760595ce3
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-01-27 22:28:25 -0500

    switch for operation options

--- script.js
+++ script.js
@@ -24,15 +24,46 @@ class Calculator {
     }
 
     chooseOperation(operation){
-
+        if (this.currentOperand == '') 
+            return;
+        if (this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
 
     compute() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current))
+            return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break;
+            case '-':
+                computation = prev - current
+                break;
+            case 'X':
+                computation = prev * current
+                break;
+            case '÷':
+                computation = prev / current
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
     }
 }
 
@@ -58,3 +89,8 @@ operators.forEach(button => {
         calculator.updateDisplay()
     })
 });
+
+equals.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
