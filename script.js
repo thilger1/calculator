22eb9babca213210727f42955519edf3e2d60ba8
thilger1 <79420353+thilger1@users.noreply.github.com>
2023-01-27 21:34:51 -0500

    Fix null operand problem

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 class Calculator {
-    constructor(previousOp, currentOp){
-        this.previousOp = previousOp;
-        this.currentOp = currentOp;
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
     }
 
     clear(){
-        this.currentOp = '';
-        this.previousOp = '';
+        this.currentOperand = '';
+        this.previousOperand = '';
         this.operation = undefined;
     }
 
     appendNumber(number){
 
+        if (this.currentOperand == null){
+            this.currentOperand = number;
+        }
+        else {
+            if (number === '.' && this.currentOperand.includes('.')) 
+                return;
+        
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+        }
     }
 
     chooseOperation(operation){
@@ -23,24 +32,29 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
-
-
 
 const numbers = document.querySelectorAll('[data-number');
 const operators = document.querySelectorAll('[data-operation');
 const equals = document.querySelector('[data-equals');
 const clear = document.querySelector('[data-clear');
-const currentOp = document.querySelector('[data-current-operand');
-const previousOp = document.querySelector('[data-previous-operand');
+const currentOperandTextElement = document.querySelector('[data-current-operand');
+const previousOperandTextElement = document.querySelector('[data-previous-operand');
 
-const calculator = new Calculator(previousOp, currentOp)
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 numbers.forEach(button => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
+        calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
-})
+});
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+});
